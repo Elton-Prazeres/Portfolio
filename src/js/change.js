c3b8c689ea7bf2755acf6fefb776c4b5813ca346
Elton-Prazeres <elton_hime20@hotmail.com>
2025-02-27 23:26:14 -0300

    linha 1-8 código do evento que altera cor do modo dark

---
+++ src/js/change.js
@@ -0,0 +1,8 @@
+// Modo Dark e Modo Light
+let trilho = document.getElementById('trilho')
+let body = document.querySelector('body')
+
+trilho.addEventListener('click', ()=>(
+    trilho.classList.toggle('dark'),
+    body.classList.toggle('dark')
+))
